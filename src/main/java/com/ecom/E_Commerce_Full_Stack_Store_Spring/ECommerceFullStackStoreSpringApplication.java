5d7c0c7973dc85964e8b50e4d7484a6715779f65
swathireddy369 <swathiamaravadi53@gmail.com>
2024-11-21 15:37:58 -0500

    Intializing the project

---
+++ src/main/java/com/ecom/E_Commerce_Full_Stack_Store_Spring/ECommerceFullStackStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.E_Commerce_Full_Stack_Store_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFullStackStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceFullStackStoreSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/E_Commerce_Full_Stack_Store_Spring/ECommerceFullStackStoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.E_Commerce_Full_Stack_Store_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceFullStackStoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
